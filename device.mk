e5e67701e4919b350441d30417c456e4f07a98a6
Dante <266556095+dpxdoss@users.noreply.github.com>
2026-07-15 04:37:43 +0530

    Update power-libperfmgr path in device.mk
    Signed-off-by: Dante <266556095+dpxdoss@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -344,7 +344,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek/libmtkperf_client \
     hardware/mediatek/libaedv \
     hardware/mediatek/wlan/wifi_hal \
-    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/clover/interfaces/power-libperfmgr \
     $(DEVICE_PATH)
 
 # Text classifier
