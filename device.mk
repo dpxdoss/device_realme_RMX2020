17955068de944d8515826acb14c8afd1eca644ed
Dante <266556095+dpxdoss@users.noreply.github.com>
2026-07-15 16:20:07 +0530

    Add Evolution X overlays to device.mk
    Signed-off-by: Dante <266556095+dpxdoss@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -321,6 +321,10 @@ PRODUCT_PACKAGES += \
     TetheringResOverlayRMX2020 \
     WifiResOverlayRMX2020
 
+# Evolution X Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+overlay-evolution
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/seccomp/,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy)
