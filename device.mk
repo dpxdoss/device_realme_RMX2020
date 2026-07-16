69147d82d476c800b349dbb581e88c3f8ff20ba4
Dante <266556095+dpxdoss@users.noreply.github.com>
2026-07-16 15:53:53 +0530

    Remove Evolution X overlays section
    Removed Evolution X overlays from device.mk.
    
    Signed-off-by: Dante <266556095+dpxdoss@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -321,10 +321,6 @@ PRODUCT_PACKAGES += \
     TetheringResOverlayRMX2020 \
     WifiResOverlayRMX2020
 
-# Evolution X Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-overlay-evolution
-
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/seccomp/,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy)
